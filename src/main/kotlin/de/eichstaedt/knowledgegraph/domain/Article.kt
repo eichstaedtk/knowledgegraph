2118e6fca710fb7b67096a45affaf109b30482ed
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2019-03-05 21:32:46 +0100

    Add more functions to article

--- src/main/kotlin/de/eichstaedt/knowledgegraph/domain/Article.kt
+++ src/main/kotlin/de/eichstaedt/knowledgegraph/domain/Article.kt
@@ -1,5 +1,6 @@
 package de.eichstaedt.knowledgegraph.domain
 
+import java.net.URL
 import java.time.LocalDateTime
 
 /**
@@ -8,4 +9,14 @@ import java.time.LocalDateTime
 data class Article(val title:String,val describtion: String, val author: String,val creationDate:LocalDateTime?= LocalDateTime.now()) {
 
     val relations:List<Relationship> = ArrayList()
+
+    val webLinks:List<URL> = ArrayList()
+
+    fun connectArticleRelation(relation:Relationship) {
+        relations.plus(relation)
+    }
+
+    fun connectToWebLink(link:URL) {
+        webLinks.plus(link)
+    }
 }
