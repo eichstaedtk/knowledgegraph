0bfb009db6778dca226370ce5f5de64ec1e6b287
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2019-03-11 21:27:30 +0100

    Add Text for Relationship

--- src/main/kotlin/de/eichstaedt/knowledgegraph/domain/Relationship.kt
+++ src/main/kotlin/de/eichstaedt/knowledgegraph/domain/Relationship.kt
@@ -3,7 +3,7 @@ package de.eichstaedt.knowledgegraph.domain
 /**
  * Created by konrad.eichstaedt@gmx.de on 22.02.19.
  */
-class Relationship(val source: Article, val target: Article, val type: Type) {
+    class Relationship(val source: Article, val target: Article, val type: Type) {
 
     enum class Type { USED_FOR, SYNONYM, BORDER_TERM, NARROWED_TERM, RELATED_TERM, TOP_TERM }
 
