f27bc7ef214f1e30547704f29e684eb38f0784cf
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2019-03-11 21:49:00 +0100

    Add new Test Article

--- src/main/kotlin/de/eichstaedt/knowledgegraph/domain/ArticleViewService.kt
+++ src/main/kotlin/de/eichstaedt/knowledgegraph/domain/ArticleViewService.kt
@@ -19,10 +19,18 @@ class ArticleViewService : ViewBoundary {
         val article = Article("Kotlin als Alternative zu Java", "Kotline ist leichtgewichtiger", "konrad")
         article.connectToWebLink(Weblink("Kotlin", URL("https://kotlinlang.org/")))
 
-
         val article1 = Article("Clean Code", "Qualitätsstandards für Softwareentwickler", "konrad")
         article1.connectToWebLink(Weblink("Clean Code", URL("https://clean-code-developer.de/")))
 
-        return Arrays.asList(article, article1)
+        val article2 = Article("Microservices", "Verteilte Architektur für Enterprise Systeme", "konrad")
+        article2.connectToWebLink(Weblink("Martin Fowler Microservices", URL("https://martinfowler.com/articles/microservices.html")))
+
+        val article3 = Article("Self Contained System", "Architekturansatz tzr Trennung von Funktionalitäten", "konrad")
+        article3.connectToWebLink(Weblink("Self Contained System", URL("https://scs-architecture.org/")))
+
+        val article4 = Article("Resource Oriented Client Architecture", "A collection of simple recommandations for decent Web Applications", "konrad")
+        article4.connectToWebLink(Weblink("ROCA Style", URL("https://roca-style.org/")))
+
+        return Arrays.asList(article, article1,article2,article3,article4)
     }
 }
