71dbf84f53f60637c6c0b916eb5ae27586923035
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2019-03-12 17:37:16 +0100

    Add Request Parameter for title

--- src/main/kotlin/de/eichstaedt/knowledgegraph/application/ArticleController.kt
+++ src/main/kotlin/de/eichstaedt/knowledgegraph/application/ArticleController.kt
@@ -1,11 +1,14 @@
 package de.eichstaedt.knowledgegraph.application
 
 import de.eichstaedt.knowledgegraph.domain.ViewBoundary
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 
 /**
@@ -15,20 +18,33 @@ import org.springframework.web.bind.annotation.GetMapping
 @Controller
 class ArticleController() {
 
+    private val logger: Logger = LoggerFactory.getLogger("ArticleController");
+
     @Autowired
     lateinit var viewService: ViewBoundary
 
     @GetMapping("/")
-    fun showArticle(model: Model): String {
+    fun showArticle(model: Model, @RequestParam("title", required = false, defaultValue = "") title: String): String {
+
+        logger.info("GET all articles")
 
         model["title"] = "Finde dein Wissen ...";
 
-        model["articles"] = viewService.findTestArticle()
+        when (title) {
+
+            "" -> {
+                logger.info("No Title Found ... ")
+                model["articles"] = viewService.findTestArticle()
+            }
+
+            else -> {
+                model["articles"] = viewService.findArticleByName(title)
+            }
+        }
 
         model["relations"] = viewService.findTestRelations()
 
         return "article";
-
     }
 
 }
